0ce8e7bd70da3a8636f1b84c13a4301f5aba7a37
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-26 10:09:04 +0100

    adding third test method for constructor of Exercise2 class

--- src/test/java/Exercise2Test.java
+++ src/test/java/Exercise2Test.java
@@ -2,10 +2,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Exercise2Test {
-
-
+    
     @Test
-    public void should_return_true_for_type_of_field_in_generic_class_and_type_of_parameter_pass_into_constructor(){
+    public void should_return_true_for_type_of_field_in_generic_class_and_type_of_parameter_pass_into_constructor() {
         //arrange
         String type1 = "test value";
         char[] type2 = new char[]{'t', 'e', 's', 't'};
@@ -16,7 +15,7 @@ public class Exercise2Test {
     }
 
     @Test
-   public void should_return_true_for_type_of_field_in_generic_class_and_type_of_parameter_pass_into_constructor2(){
+    public void should_return_true_for_type_of_field_in_generic_class_and_type_of_parameter_pass_into_constructor3() {
         //arrange
         Integer integer = 2;
         Object object = new Object();
@@ -24,5 +23,16 @@ public class Exercise2Test {
 
         //assert
         Assert.assertTrue(exercises2.filedTypeT.equals(integer) && exercises2.fieldTypeS == object);
+    }
+
+    @Test
+    public void should_return_true_for_type_of_field_in_generic_class_and_type_of_parameter_pass_into_constructor2() {
+        //arrange
+        float num = 2.2f;
+        Thread currentThread = Thread.currentThread();
+        Exercises2<Float, Thread> exercises2 = new Exercises2<Float, Thread>(num, currentThread);
+
+        //assert
+        Assert.assertTrue(exercises2.filedTypeT.equals(num) && exercises2.fieldTypeS == currentThread);
     }
 }
