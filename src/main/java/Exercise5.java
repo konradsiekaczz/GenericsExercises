1d3606048b17c61d7fd68311bfdf7953552b8ea9
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-26 12:32:33 +0100

    adding method with upper bound and lower bound

--- src/main/java/Exercise5.java
+++ src/main/java/Exercise5.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Exercise5 {
 
     static class Singer {
-        private final String genreMusic;
+        private String genreMusic;
 
         public Singer(String genreMusic) {
             this.genreMusic = genreMusic;
@@ -15,10 +19,26 @@ public class Exercise5 {
         }
     }
 
-    static class ElvisPresley extends Exercise4.Singer {
+    static class ElvisPresley extends Singer {
 
         public ElvisPresley() {
             super("Rock 'n' Roll");
         }
+    }
+
+    private static void doSomethingWithSinger(List<? extends Singer> singers, Consumer<? super Singer> consumer) {
+        for (Singer singer : singers) {
+            consumer.accept(singer);
+        }
+    }
+
+    public static void main(String[] args) {
+        Consumer<Singer> changeToPopStar = singer -> singer.genreMusic = "KPop";
+        List<Singer> singers = Arrays.asList(new ElvisPresley());
+        doSomethingWithSinger(singers, changeToPopStar);
+        System.out.println(singers.get(0).genreMusic);
+
+        Consumer<Object> print = System.out::print;
+        doSomethingWithSinger(singers, print);
     }
 }
