9d7c6584bcea8bde49c6d24faf501ce2137cdbb2
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-26 09:54:04 +0100

    adding test for constructor in class Exercise2

--- src/test/java/Exercise2Test.java
+++ src/test/java/Exercise2Test.java
@@ -1,2 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Exercise2Test {
+
+    public static void main(String[] args) {
+        Exercise2Test exercise2Test = new Exercise2Test();
+
+        exercise2Test.test_exercise2_class_with_two_parametrized_type();
+    }
+
+    @Test
+    public void test_exercise2_class_with_two_parametrized_type(){
+        //arrange
+        String type1 = "test value";
+        char[] type2 = new char[]{'t', 'e', 's', 't'};
+        Exercises2<String, char[]> exercises2 = new Exercises2<String, char[]>(type1, type2);
+
+        //assert
+        Assert.assertTrue(exercises2.filedTypeT.equals(type1) && exercises2.fieldTypeS == type2);
+    }
 }
