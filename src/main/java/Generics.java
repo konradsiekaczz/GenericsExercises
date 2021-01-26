d019fd4c8e598487b86d376947e209f88e06ea01
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-26 09:14:03 +0100

    adding method without using Generics

--- src/main/java/Generics.java
+++ src/main/java/Generics.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generics {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void retroJavaStyle(){
+        List strings = new ArrayList();
+
+        strings.add("first");
+        strings.add("second");
+
+        strings.add(new Integer(3));
+
+        String string  = (String) strings.get(0);
+    }
 }
