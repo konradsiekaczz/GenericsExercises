0a6d468b333e6e9bdfb44fbdb59ac71bc605de51
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-26 11:48:49 +0100

    adding exercise 3 with inheritance and parametrized methods

---
+++ src/main/java/Exercise3.java
@@ -0,0 +1,37 @@
+public class Exercise3 {
+
+    interface Clonable<T extends Clonable>{
+        T clone();
+    }
+
+    static class Singer {
+    }
+
+    static class ElvisPresley extends Singer implements Clonable<ElvisPresley>{
+
+        @Override
+        public ElvisPresley clone() {
+            return new ElvisPresley();
+        }
+
+        @Override
+        public String toString() {
+            return "Elvis Presley";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(cloneSinger(new ElvisPresley()));
+    }
+
+    private static <T extends Singer & Clonable> Clonable cloneSinger(Clonable clonable){
+        return clonable.clone();
+    }
+
+    private static <T> T returnT(T t){
+        System.out.println(t);
+        return t;
+    }
+
+
+}
