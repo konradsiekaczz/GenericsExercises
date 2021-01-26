8cec40eb373b3eab91d563b13fe5525a490083fb
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-26 09:26:37 +0100

    adding method with parametrized list

--- src/main/java/Generics.java
+++ src/main/java/Generics.java
@@ -4,10 +4,10 @@ import java.util.List;
 public class Generics {
 
     public static void main(String[] args) {
-
+        retroJavaStyle();
     }
 
-    static void retroJavaStyle(){
+    static void retroJavaStyle() {
         List strings = new ArrayList();
 
         strings.add("first");
@@ -15,6 +15,27 @@ public class Generics {
 
         strings.add(new Integer(3));
 
-        String string  = (String) strings.get(0);
+        String string = (String) strings.get(0);
+
+        try {
+
+            String anInteger = (String) strings.get(2); //illegal casting, we don't have defined what type we
+            // can add to list.
+        } catch (ClassCastException e) {
+            System.out.println("Illegal casting!");
+        }
+    }
+
+    static  void  modernJavaStyle(){
+        List<String> stringList = new ArrayList<String>();
+
+        stringList.add("first");
+        stringList.add("second");
+
+//        stringList.add(new Integer(3));// Compilation error!
+
+        //stringList.add(2); //this is parametrized list, we can't put int in to it.
+
+        String s = stringList.get(0);  //We dont need to cast.
     }
 }
